d0a018c450499bf71a8b077e3d4bdb5cc01a5a3d
WeiKing1021 <WeiKing1021@gmail.com>
2022-05-10 09:03:48 +0800

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,95 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {WebcamImage, WebcamInitError, WebcamUtil} from "ngx-webcam";
+import {Observable, Subject} from "rxjs";
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ShoppingCar';
+
+  @Output() getPicture = new EventEmitter<WebcamImage>();
+  showWebcam = true;
+  isCameraExist = true;
+
+  errors: WebcamInitError[] = [];
+
+  // webcam snapshot trigger
+  private trigger: Subject<void> = new Subject<void>();
+  private nextWebcam: Subject<boolean | string> = new Subject<boolean | string>();
+
+  public totalPrice = 0;
+
+  constructor(private http: HttpClient) {
+
+    this.getPicture.subscribe((next) => {
+      // let blob = this.convertToBlob(next.imageAsBase64);
+      let imageData = this.base64ToArrayBuffer(next.imageAsBase64);
+
+      http.post('https://python.weicraft.tw/addToCart', imageData).subscribe(result => {
+
+        let resultData = (<any>result);
+
+        alert('物品: ' + resultData.itemName + ' (' + resultData.price + '元)');
+
+        this.totalPrice = resultData.totalPrice;
+      });
+    });
+  }
+
+  public clear(): void {
+
+    this.http.get('https://python.weicraft.tw/reset').subscribe(console.log);
+
+    this.totalPrice = 0;
+  }
+
+  private base64ToArrayBuffer(base64: string): any {
+    let binary_string = window.atob(base64);
+    let len = binary_string.length;
+    let bytes = new Uint8Array(len);
+    for (let i = 0; i < len; i++) {
+      bytes[i] = binary_string.charCodeAt(i);
+    }
+    return bytes.buffer;
+  }
+
+  ngOnInit(): void {
+    WebcamUtil.getAvailableVideoInputs()
+      .then((mediaDevices: MediaDeviceInfo[]) => {
+        this.isCameraExist = mediaDevices && mediaDevices.length > 0;
+      });
+  }
+
+  takeSnapshot(): void {
+    this.trigger.next();
+  }
+
+  onOffWebCame() {
+    this.showWebcam = !this.showWebcam;
+  }
+
+  handleInitError(error: WebcamInitError) {
+    this.errors.push(error);
+  }
+
+  changeWebCame(directionOrDeviceId: boolean | string) {
+    this.nextWebcam.next(directionOrDeviceId);
+  }
+
+  handleImage(webcamImage: WebcamImage) {
+    this.getPicture.emit(webcamImage);
+    // this.showWebcam = false;
+  }
+
+  get triggerObservable(): Observable<void> {
+    return this.trigger.asObservable();
+  }
+
+  get nextWebcamObservable(): Observable<boolean | string> {
+    return this.nextWebcam.asObservable();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { WebcamModule } from 'ngx-webcam';
+import {HttpClientModule} from "@angular/common/http";
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WebcamModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
